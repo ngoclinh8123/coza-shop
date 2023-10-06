38d04799c275a796df1d1a2dcf54ed836175795b
ngoclinh8123 <ngoclinh8123@gmail.com>
2023-10-06 11:57:54 +0700

    change database info

--- modules/handle/connect-database.php
+++ modules/handle/connect-database.php
@@ -3,10 +3,10 @@
     // $username="epiz_32686373";
     // $password="gAna6zhy0Ame";
     // $dbname="epiz_32686373_sheeta";
-    $server="localhost";
+    $server="127.0.0.1";
     $username="root";
-    $password="Ngoclinh08012003";
-    $dbname="cozastore2";
+    $password="123";
+    $dbname="coza_shop";
     // ini_set('display_errors',1);
 
     $connect=mysqli_connect($server,$username,$password,$dbname);
